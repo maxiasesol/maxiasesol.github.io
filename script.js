42049b02eff0d926ba620e07338ab011d8e783d5
Maximiliano Asesol <asesolmaximiliano@gmail.com>
2023-08-09 15:26:53 -0300

    quito el fade in hasta que funcione bien

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ header.addEventListener("click", function(){
 
 })
 
-w.addEventListener("scroll",e=>{
-    let scrolltop = w.pageYOffset;
-    if(scrolltop>250 & scrolltop<1200){sobreMi.classList.add("fadein");}
-    else{sobreMi.classList.remove("fadein");}
+// w.addEventListener("scroll",e=>{
+//     let scrolltop = w.pageYOffset;
+//     if(scrolltop>250 & scrolltop<1200){sobreMi.classList.add("fadein");}
+//     else{sobreMi.classList.remove("fadein");}
 
-    if(scrolltop>950 & scrolltop<1700){proyectos.classList.add("fadein");}
-    else{proyectos.classList.remove("fadein");}
+//     if(scrolltop>950 & scrolltop<1700){proyectos.classList.add("fadein");}
+//     else{proyectos.classList.remove("fadein");}
 
 
-});
+// });
 
 const btn = document.getElementById('button');
 
@@ -35,17 +35,17 @@ document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.value = 'Enviando...';
 
    const serviceID = 'default_service';
    const templateID = 'template_igqllcm';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
+      btn.value = 'enviando';
+      alert('Enviado exitosamente');
     }, (err) => {
-      btn.value = 'Send Email';
+      btn.value = 'enviando';
       alert(JSON.stringify(err));
     });
 });
