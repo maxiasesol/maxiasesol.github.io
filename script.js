137cd613e4e14a5adf4804804ac292d55eb92281
Maximiliano Asesol <asesolmaximiliano@gmail.com>
2023-08-09 10:14:51 -0300

    se cambio contacto
    ds

--- script.js
+++ script.js
@@ -29,4 +29,23 @@ w.addEventListener("scroll",e=>{
 
 });
 
+const btn = document.getElementById('button');
 
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_igqllcm';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
